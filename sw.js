9428cb711c7f7889ea68f6800aa362273ebb2aa7
farjiwala <githuband27@gmail.com>
2026-08-20 09:38:44 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,54 +1,27 @@
-const CACHE_NAME="hayger-ultra-v5";
-const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "hayger-ultra-v6";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png"
+];
 
-self.addEventListener("install",event=>{
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache=>cache.addAll(APP_SHELL))
-      .then(async()=>{
-        // First install activates immediately; later updates wait for the user.
-        const clients=await self.clients.matchAll({type:"window",includeUncontrolled:true});
-        if(!clients.length) return self.skipWaiting();
-      })
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE_NAME).then((c) => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k))
+      );
+    })
   );
 });
 
-self.addEventListener("activate",event=>{
-  event.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(
-        keys.filter(k=>k.startsWith("hayger-")&&k!==CACHE_NAME).map(k=>caches.delete(k))
-      ))
-      .then(()=>self.clients.claim())
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
   );
-});
-
-self.addEventListener("fetch",event=>{
-  if(event.request.method!=="GET") return;
-
-  if(event.request.mode==="navigate"){
-    event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          if(response.ok) caches.open(CACHE_NAME).then(cache=>cache.put(event.request,response.clone()));
-          return response;
-        })
-        .catch(()=>caches.match("./index.html"))
-    );
-  }else{
-    event.respondWith(
-      fetch(event.request)
-        .then(response=>{
-          if(response.ok&&response.type!=="opaque"){
-            caches.open(CACHE_NAME).then(cache=>cache.put(event.request,response.clone()));
-          }
-          return response;
-        })
-        .catch(()=>caches.match(event.request))
-    );
-  }
-});
-
-self.addEventListener("message",event=>{
-  if(event.data==="SKIP_WAITING") self.skipWaiting();
 });
