55218f9830a4bdaabcf06a6197fa119b34077adc
farjiwala <githuband27@gmail.com>
2026-09-10 18:10:58 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "hayger-final-v2.2-polish";
+const CACHE_NAME = "hayger-final-v2.3-top-class";
 const APP_SHELL=["./","./index.html","./manifest.json","./icon-192.png","./icon-512.png"];
 
 self.addEventListener("install",event=>{
